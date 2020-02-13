53eeb3f79a43c0d3ceb15186defa525e90713d6f
Khoa Vo <khoa.vo@verizonmedia.com>
2020-02-13 01:25:17 -0800

    Render mobile header and nav

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -1,3 +1,25 @@
 document.getElementById('work-nav').onclick = function () {
     document.getElementById('work-subnav').classList.toggle('show');
 }
+
+document.getElementById('hamburger').onclick = function () {
+    const navList = document.getElementById('nav-list-container');
+    const nav = document.getElementById('mobile-nav');
+    const hamburgerIcon = document.getElementById('hamburger-icon');
+    const closeIcon = document.getElementById('close-icon');
+    navList.classList.toggle('shown');
+
+    if (navList.classList.contains('shown')) {
+        nav.style.boxShadow = '0 0 8px #949494';
+        nav.style.height = 'auto';
+        nav.style.paddingRight = '40px';
+        hamburgerIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
+    } else {
+        nav.style.boxShadow = 'none';
+        nav.style.height = '100%';
+        nav.style.paddingRight = '20px';
+        closeIcon.style.display = 'none';
+        hamburgerIcon.style.display = 'block';
+    }
+}
